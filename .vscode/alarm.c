92d3753513db83d22fa8c51129e10b9254fca8f7
kjun0712 <kjun0712@gmail.com>
2024-04-03 15:08:11 +0900

    알람시계

--- .vscode/alarm.c
+++ .vscode/alarm.c
@@ -3,11 +3,11 @@
 int main(){
     int a,b;
     scanf("%d %d", &a, &b);
-    if(b>=45&&b<=59&&0<=a<=23){
-        printf("%d %d",a,b-45);
-    }else if(b<45&&b>=0&&0<a<=23){
-        printf("%d %d",a-1,b+15);
-    }else if(b<45&&b>=0&&a==0){
-        printf("%d %d",a+23,b+15);
+    b-=45;
+    if(b<0){
+        b+=60;
+        a--;
+        if(a<0) a+=24;
     }
+    printf("%d %d",a, b);
 }
