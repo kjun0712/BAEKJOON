2a7978b037bdd5acd20c0dfe4f493adaa79299f3
kjun0712 <kjun0712@gmail.com>
2024-04-05 18:52:09 +0900

    오븐시계

--- .vscode/oven.c
+++ .vscode/oven.c
@@ -2,12 +2,12 @@
 
 int main(){
     int a,b,c;
-    scanf("%d %d", &a, &b, &c);
+    scanf("%d %d %d", &a, &b, &c);
     b+=c;
     if(b>=60){
-        b-=60;
-        a++;
-        if(a>=0) a-=24;
+        a+=b/60;
+        b%=60;
+        if(a>=24) a-=24;
     }
     printf("%d %d",a, b);
 }
