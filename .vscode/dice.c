2f98b742c4244c524160b8160b57ccdaba2f38af
kjun0712 <kjun0712@gmail.com>
2024-04-05 20:04:28 +0900

    주사위 세개

--- .vscode/dice.c
+++ .vscode/dice.c
@@ -2,13 +2,27 @@
 
 int main(){
     int a,b,c;
+    int max;
     scanf("%d %d %d",&a,&b,&c);
 
     if(a=b=c){
         printf('%d', 10000+a*1000);
     }else if(a==b ||a==c ||b==c){
-        printf('%d', 10000+a*1000);
+        if(a==c){
+            printf('%d', 10000+a*1000);
+        }else if(a==b){
+            printf('%d', 1000+a*100);
+        }else{
+            printf('%d', 1000+b*100);
+        }
     }else{
-        printf('%d', 10000+a*1000);
+        if(max<b){
+            max=b;
+        }else if(max<c){
+            max=c;
+        }else{
+            max=a;
+        }
+        printf('%d', max*100);
     }
 }
