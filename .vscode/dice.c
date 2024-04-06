71b05c8af6893cf1ce81419dccb1ffc6d6be879b
kjun0712 <kjun0712@gmail.com>
2024-04-06 11:45:52 +0900

    주사위 세개

--- .vscode/dice.c
+++ .vscode/dice.c
@@ -1,29 +1,25 @@
 #include<stdio.h>
 
 int main(){
-    int a,b,c,p;
+    int a,b,c;
     int max;
     scanf("%d %d %d",&a,&b,&c);
 
-    if(a==b==c){
-        p=10000+a*1000;
-        printf("%d", p);
-    }else if(a==c!=b){
-        p=1000+a*100;
-        printf("%d", p);
-    }else if(a==b!=c){
-        p=1000+a*100;
-        printf("%d", p);
-    }else if(b==c!=a){
+    if(a==b&&b==c){
+        printf("%d", 10000+a*1000);
+    }else if(a==c&&c!=b){
+        printf("%d", 1000+a*100);
+    }else if(a==b&&a!=c){
+        printf("%d", 1000+a*100);
+    }else if(b==c&&c!=a){
         printf("%d", 1000+b*100);
     }else{
-        max=a;
-        if(max<b){
-            max=b;
-        }else if(max<c){
-            max=c;
-        }else{
+        if(c<a&&b<a){
             max=a;
+        }else if(c<b&&a<b){
+            max=b;
+        }else{
+            max=c;
         }
         printf("%d", max*100);
     }
