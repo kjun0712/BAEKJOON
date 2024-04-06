d3df91ff6ca618815fa04a13c2055e9b1d5e99cf
kjun0712 <kjun0712@gmail.com>
2024-04-07 02:47:59 +0900

    A+B 여러번 출력

--- .vscode/A+B_for.c
+++ .vscode/A+B_for.c
@@ -4,10 +4,10 @@ int main(){
     int a,b,c;
     scanf("%d",&a);
 
-    for(int i=1;i==a;i++){
+    for(int i=1;i<a+1;i++){
         int b,c;
         scanf("%d %d",&b,&c);
 
-        printf("%d",b+c);
+        printf("%d\n",b+c);
     }
 }
