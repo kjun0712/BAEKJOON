6373964d5b04c9d27a91f6c3797059d14ee4186f
kjun0712 <kjun0712@gmail.com>
2024-04-06 12:13:32 +0900

    더하기 반복

---
+++ .vscode/A+B_for.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,c;
+    scanf("%d",&a);
+
+    for(int i=1;i==a;i++){
+        int b,c;
+        scanf("%d %d",&b,&c);
+
+        printf("%d",b+c);
+    }
+}
