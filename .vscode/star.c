70adffd8a3116dccda4ee79351ce06bfc247a3d5
kjun0712 <kjun0712@gmail.com>
2024-04-08 23:31:28 +0900

    별찍기

---
+++ .vscode/star.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    scanf("%d",&a);
+    for(int i=0;i<a;i++){
+        for(int t=0;t<=i;t++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
