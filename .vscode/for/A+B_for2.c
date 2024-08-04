3e658ed2ad647570c870808c91337320ac5a8558
kjun0712 <kjun0712@gmail.com>
2024-08-04 11:14:01 +0900

    .

---
+++ .vscode/for/A+B_for2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+
+    for(int i=0;i==a+b;){
+        scanf("%d %d",&a,&b);
+        if(a==b==0){
+            printf("%d %d",a,b);
+        }else{
+            printf("%d\n",a+b);
+        }
+    }
+}
--- helloWorld.py
+++
@@ -1,3 +0,0 @@
-#hello WOrld!
-
-print("Hello World!")
